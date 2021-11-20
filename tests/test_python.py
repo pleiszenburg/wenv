b29023c0305add38e91093db3766da1e1f494d94
Sebastian M. Ernst <ernst@pleiszenburg.de>
2021-11-20 15:59:07 +0100

    black

--- tests/test_python.py
+++ tests/test_python.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 
 """
-
 WENV
 Running Python on Wine
 https://github.com/pleiszenburg/wenv
 
-	tests/test_util.py: Testing Python interpreter
+    tests/test_util.py: Testing Python interpreter
 
-	Copyright (C) 2017-2020 Sebastian M. Ernst <ernst@pleiszenburg.de>
+    Copyright (C) 2017-2020 Sebastian M. Ernst <ernst@pleiszenburg.de>
 
 <LICENSE_BLOCK>
 The contents of this file are subject to the GNU Lesser General Public License
@@ -21,9 +20,7 @@ Software distributed under the License is distributed on an "AS IS" basis,
 WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
 specific language governing rights and limitations under the License.
 </LICENSE_BLOCK>
-
 """
-
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # IMPORT
@@ -37,20 +34,22 @@ import pytest
 # TEST(s)
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-@pytest.mark.parametrize('arch', get_context())
+@pytest.mark.parametrize("arch", get_context())
 def test_python(arch):
 
-	out, err, code = run_process(['wenv', 'python', '-m', 'platform'], env = {'WENV_ARCH': arch})
+    out, err, code = run_process(
+        ["wenv", "python", "-m", "platform"], env={"WENV_ARCH": arch}
+    )
 
-	assert code == 0
-	assert len(err.strip()) == 0
-	assert 'Windows' in out
+    assert code == 0
+    assert len(err.strip()) == 0
+    assert "Windows" in out
 
-	out, err, code = run_process(
-		['wenv', 'python', '-c', 'import platform; print(platform.machine())'],
-		env = {'WENV_ARCH': arch}
-		)
-	assert code == 0
-	assert len(err.strip()) == 0
-	out = out.strip()
-	assert (arch == 'win32' and out == 'x86') ^ (arch == 'win64' and out == 'AMD64')
+    out, err, code = run_process(
+        ["wenv", "python", "-c", "import platform; print(platform.machine())"],
+        env={"WENV_ARCH": arch},
+    )
+    assert code == 0
+    assert len(err.strip()) == 0
+    out = out.strip()
+    assert (arch == "win32" and out == "x86") ^ (arch == "win64" and out == "AMD64")
