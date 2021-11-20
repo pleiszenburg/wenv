adc1cb81002327543bb8a4897acd35e4d6b902e9
Sebastian M. Ernst <ernst@pleiszenburg.de>
2021-11-20 15:57:46 +0100

    tests/test_init.py

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 
 """
-
 WENV
 Running Python on Wine
 https://github.com/pleiszenburg/wenv
 
-	tests/test_init.py: Testing clean & init
+    tests/test_init.py: Testing clean & init
 
-	Copyright (C) 2017-2020 Sebastian M. Ernst <ernst@pleiszenburg.de>
+    Copyright (C) 2017-2020 Sebastian M. Ernst <ernst@pleiszenburg.de>
 
 <LICENSE_BLOCK>
 The contents of this file are subject to the GNU Lesser General Public License
@@ -21,9 +20,7 @@ Software distributed under the License is distributed on an "AS IS" basis,
 WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
 specific language governing rights and limitations under the License.
 </LICENSE_BLOCK>
-
 """
-
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # IMPORT
@@ -37,62 +34,66 @@ import pytest
 # TEST(s)
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-@pytest.mark.parametrize('arch', get_context())
+
+@pytest.mark.parametrize("arch", get_context())
 def test_init(arch):
 
-	out, err, code = run_process(['wenv', 'clean'], env = {'WENV_ARCH': arch})
+    out, err, code = run_process(["wenv", "clean"], env={"WENV_ARCH": arch})
 
-	assert code == 0
-	assert len(err.strip()) == 0
+    assert code == 0
+    assert len(err.strip()) == 0
 
-	out, err, code = run_process(['wenv', 'help'], env = {'WENV_ARCH': arch})
+    out, err, code = run_process(["wenv", "help"], env={"WENV_ARCH": arch})
 
-	assert code == 0
-	assert len(err.strip()) == 0
-	assert 'wenv pip' not in out
-	# assert 'wenv python' not in out # TODO
+    assert code == 0
+    assert len(err.strip()) == 0
+    assert "wenv pip" not in out
+    # assert 'wenv python' not in out # TODO
 
-	out, err, code = run_process(['wenv', 'init'], env = {'WENV_ARCH': arch})
+    out, err, code = run_process(["wenv", "init"], env={"WENV_ARCH": arch})
 
-	assert code == 0
-	# assert len(err.strip()) == 0 # pip output goes to stderr
+    assert code == 0
+    # assert len(err.strip()) == 0 # pip output goes to stderr
 
-	out, err, code = run_process(['wenv', 'help'], env = {'WENV_ARCH': arch})
+    out, err, code = run_process(["wenv", "help"], env={"WENV_ARCH": arch})
 
-	assert code == 0
-	assert len(err.strip()) == 0
-	assert 'wenv pip' in out
-	# assert 'wenv python' in out # TODO
+    assert code == 0
+    assert len(err.strip()) == 0
+    assert "wenv pip" in out
+    # assert 'wenv python' in out # TODO
 
-@pytest.mark.parametrize('arch', get_context())
+
+@pytest.mark.parametrize("arch", get_context())
 def test_init_offline(arch):
 
-	out, err, code = run_process(['wenv', 'clean'], env = {'WENV_ARCH': arch})
+    out, err, code = run_process(["wenv", "clean"], env={"WENV_ARCH": arch})
 
-	assert code == 0
-	assert len(err.strip()) == 0
+    assert code == 0
+    assert len(err.strip()) == 0
 
-	out, err, code = run_process(['wenv', 'help'], env = {'WENV_ARCH': arch})
+    out, err, code = run_process(["wenv", "help"], env={"WENV_ARCH": arch})
 
-	assert code == 0
-	assert len(err.strip()) == 0
-	assert 'wenv pip' not in out
-	# assert 'wenv python' not in out # TODO
+    assert code == 0
+    assert len(err.strip()) == 0
+    assert "wenv pip" not in out
+    # assert 'wenv python' not in out # TODO
 
-	out, err, code = run_process(['wenv', 'cache'], env = {'WENV_ARCH': arch})
+    out, err, code = run_process(["wenv", "cache"], env={"WENV_ARCH": arch})
 
-	assert code == 0
-	assert len(err.strip()) == 0
-	assert 'wenv pip' not in out
+    assert code == 0
+    assert len(err.strip()) == 0
+    assert "wenv pip" not in out
 
-	out, err, code = run_process(['wenv', 'init'], env = {'WENV_ARCH': arch, 'WENV_OFFLINE': 'true'})
+    out, err, code = run_process(
+        ["wenv", "init"], env={"WENV_ARCH": arch, "WENV_OFFLINE": "true"}
+    )
 
-	assert code == 0
-	# assert len(err.strip()) == 0 # pip output goes to stderr
+    assert code == 0
+    # assert len(err.strip()) == 0 # pip output goes to stderr
 
-	out, err, code = run_process(['wenv', 'help'], env = {'WENV_ARCH': arch})
+    out, err, code = run_process(["wenv", "help"], env={"WENV_ARCH": arch})
 
-	assert code == 0
-	assert len(err.strip()) == 0
-	assert 'wenv pip' in out
-	# assert 'wenv python' in out # TODO
+    assert code == 0
+    assert len(err.strip()) == 0
+    assert "wenv pip" in out
+    # assert 'wenv python' in out # TODO
