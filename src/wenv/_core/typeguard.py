127f5a2b1c4abedfbccc337bebd6171234ee000f
Sebastian M. Ernst <ernst@pleiszenburg.de>
2021-11-25 11:38:28 +0100

    clarify origin of warning

--- src/wenv/_core/typeguard.py
+++ src/wenv/_core/typeguard.py
@@ -31,6 +31,6 @@ import warnings
 
 if os.environ.get('WENV_DEBUG', '0') == '1':
     from typeguard import typechecked
-    warnings.warn("running in debug mode with activated run-time type checks", RuntimeWarning)
+    warnings.warn("wenv running in debug mode with activated run-time type checks", RuntimeWarning)
 else:
     typechecked = lambda x: x
