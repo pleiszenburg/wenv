488041ea68ce27bc996993a8759438f1be551e18
Sebastian M. Ernst <ernst@pleiszenburg.de>
2022-09-11 15:44:55 +0200

    new release cycle (2)

--- src/wenv/__init__.py
+++ src/wenv/__init__.py
@@ -26,7 +26,7 @@ specific language governing rights and limitations under the License.
 # IMPORT / EXPORT
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-__version__ = '0.4.1'  # Bump version HERE!
+__version__ = '0.4.2'  # Bump version HERE!
 
 from ._core.env import (
     cli,
